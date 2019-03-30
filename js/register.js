421085f7b79e63626489ce47633534b80ff6e3fe
ShivankSahai <shivank.sahai2017@vitstudent.ac.in>
2019-03-30 23:04:53 +0530

    new

--- js/register.js
+++ js/register.js
@@ -7,40 +7,52 @@ document.querySelector('#reg_b').addEventListener('click',(e)=>{
     let password = document.querySelector('#password').value
 
     //let new_cust = {first_name:fn,last_name:ln,aadhar:acn,pnumber:phone}
-    let new_user = {email: email, password:password,pnumber:phone,first_name:fn,last_name:ln,aadhar:acn,balance:'0'}
+    let new_user = {email: email, password:password,pnumber:Number(phone),first_Name:fn,last_Name:ln,aadhar:Number(acn),balance:0}
 
     
     console.log(new_user) //dont know how to access
 
-    let ge = () => {
-            return new Promise(function (resolve, reject) {
-                let request = new XMLHttpRequest()
+    // let ge =() => {
+    //         return new Promise(function (resolve, reject) {
+    //             let request = new XMLHttpRequest()
 
-                request.addEventListener('readystatechange', (e) => {
-                    if (e.target.status === 200 && e.target.readyState === 4) {
-                        let data = JSON.parse(e.target.responseText)
-                        resolve(data)
-                    }
-                    else if (e.target.readyState === 4) {
-                        reject('something went wrong')
-                    }
-                })
-                request.open('POST', `https://rocky-chamber-58124.herokuapp.com/deployed/users`, true)
+    //             request.addEventListener('readystatechange', (e) => {
+    //                 if (e.target.status === 200 && e.target.readyState === 4) {
+    //                     let data = JSON.parse(e.target.responseText)
+    //                     resolve(data)
+    //                 }
+    //                 else if (e.target.readyState === 4) {
+    //                     reject('something went wrong')
+    //                 }
+    //             })
+    //             request.open('POST', `https://still-inlet-89790.herokuapp.com/users`, true)
+                
+    //             request.setRequestHeader('Content-type', 'application/json');
+    //             let data = JSON.stringify(new_user)
+    //             request.send(data)
+                
+    //         })
+    // }
 
-                request.setRequestHeader('Content-type', 'application/json');
-                let data = JSON.stringify(new_user)
-                request.send(data)
-            })
+    // ge().then((doc) => {
+    //     console.log(doc)
+
+    //     window.location.href = "login.html", (err) => {
+    //         console.log(err)
+    //     }
+    // }, (err) => {
+    //     console.log(err)
+    // })
+
+    axios.post('https://still-inlet-89790.herokuapp.com/users',new_user,{
+        headers:{
+            'Content-type':'application/json'
         }
+    }).then((res)=>{
+        console.log(res.data)
+        window.location.href = "login.html"
+    })
 
-        ge().then((doc) => {
-            console.log(doc)
 
-            window.location.href = "login.html", (err) => {
-                console.log(err)
-            }
-        }, (err) => {
-            console.log(err)
-        })
     }
 )
